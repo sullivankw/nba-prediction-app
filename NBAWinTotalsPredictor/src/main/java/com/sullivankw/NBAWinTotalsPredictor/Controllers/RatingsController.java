a4c622c3e196a9bec552814851acb8d3ae9de520
Kevin Sullivan <sullivankw@gmail.com>
2019-10-26 23:55:09 -0700

    controller, current only working method is "loadData"

--- NBAWinTotalsPredictor/src/main/java/com/sullivankw/NBAWinTotalsPredictor/Controllers/RatingsController.java
+++ NBAWinTotalsPredictor/src/main/java/com/sullivankw/NBAWinTotalsPredictor/Controllers/RatingsController.java
@@ -1,7 +1,36 @@
 package com.sullivankw.NBAWinTotalsPredictor.Controllers;
 
-import org.springframework.stereotype.Controller;
+import com.sullivankw.NBAWinTotalsPredictor.services.FileService;
+import com.sullivankw.NBAWinTotalsPredictor.services.ProjectionsService;
+import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+import java.io.IOException;
+import java.text.ParseException;
+
+@RestController
+@RequestMapping(value="/ratings")
 public class RatingsController {
+
+    @Autowired
+    private ProjectionsService projectionsService;
+
+    @Autowired
+    private FileService fileService;
+
+    @GetMapping(value="/wins/{team}")
+    public int getProjectedWinTotal(@PathVariable String team) {
+        return projectionsService.getWinsByTeam(team);
+    }
+
+    @GetMapping(value="/load")
+    public String loadData() throws ParseException, InvalidFormatException, IOException {
+        return fileService.extractAllData();
+    }
+
+
 }
